50cffe216950237b43981dc813a8c066f53503ce
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-06-25 23:46:50 +0530

    showed todo in a formatted manner

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
     def index
-      render plain: Todo.all.to_a
+      render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string}.join("\n")
     end
   end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,6 @@
 class Todo < ActiveRecord::Base
+    def to_pleasant_string
+      is_completed = completed ? "[x]" : "[ ]"
+      "#{id} -: #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
+    end
 end
