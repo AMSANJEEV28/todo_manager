2795f6dfd79a199129ceeb9493e5284515da8e74
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-06-27 03:10:59 +0530

    user files added

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+    def to_UserList
+      "#{id}. #{name}"
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   resources :todos
-  
+  resources :users
+  post "users/login"
 end
