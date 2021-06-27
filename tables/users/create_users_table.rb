fbc949153e43fc61222a233b8f3af2972f52b4c3
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-06-28 00:12:01 +0530

    applied migration feature

--- tables/users/create_users_table.rb
+++
@@ -1,8 +0,0 @@
-require './connect_db.rb'
-connect_db!
-
-ActiveRecord::Migration.create_table(:users) do |t|
-    t.string :name
-    t.string :email
-    t.string :password
-end
--- tables/users/user.rb
+++
@@ -1,5 +0,0 @@
-require 'active_record'
-
-class User < ActiveRecord::Base
-    
-end
