5b9c8501dbe7ee516a2dd7d8c2ecd73852d9d87f
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-07-03 21:29:17 +0530

    Combining the design-based HTML & CSS with ERB to render todos from the database

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,37 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_completed = completed ? "[x]" : "[ ]"
-    "#{id} -: #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
+
+  def self.show_list
+    puts "My Todo-List\n\n\tOverdue\n\n"
+    puts overdue.map { |todo| todo.to_displayable_string }
+    puts "\n\tDue Today\n\n"
+    puts due_today.map { |todo| todo.to_displayable_string }
+    puts "\n\tDue Later\n\n"
+    puts due_later.map { |todo| todo.to_displayable_string }
+  end
+
+  def self.mark_as_complete(id)
+    todo = Todo.find(id)
+    todo.completed = true
+    todo.save
+    todo
   end
 end
