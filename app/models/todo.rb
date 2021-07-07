e01f0b0c5f79b33818cfdd0d83200ec69ecb8307
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-07-08 02:42:18 +0530

    updated to fully functional todo page

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -4,7 +4,7 @@ class Todo < ActiveRecord::Base
   end
 
   def self.overdue
-    where("due_date < ? and (not completed)", Date.today).ordered
+    where("due_date < ?", Date.today)
   end
 
   def self.due_today
