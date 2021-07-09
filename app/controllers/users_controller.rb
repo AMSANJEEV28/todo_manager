fc69d08e4a57b6e59bef5f3761340ca19e891549
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-07-09 16:36:23 +0530

    user signup created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 
     new_user = User.create!(
       first_name: first_name,
-      last_name: last_name
+      last_name: last_name,
       email: email,
       password_digest: password_digest,
     )
