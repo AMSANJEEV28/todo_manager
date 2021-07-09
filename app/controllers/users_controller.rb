58d1ddf807c08705f4a160862485eec352894f21
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-07-09 17:03:18 +0530

    password digest added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,26 +3,12 @@ def new
   render "users/new"
   end
 
-  def index
-    render plain: User.all.map { |user| user.to_user_list }.join("\n")
-  end
-
-  def show
-    id = params[:id]
-    users = User.find(id)
-    render plain: users.to_user_list
-  end
-
   def create
+    User.create!(
     first_name = params[:first_name]
     last_name = params[:last_name]
     email = params[:email]
-
-    new_user = User.create!(
-      first_name: first_name,
-      last_name: last_name,
-      email: email,
-      password_digest: password_digest,
+    password: params[:password],
     )
     redirect_to  "/"
   end
