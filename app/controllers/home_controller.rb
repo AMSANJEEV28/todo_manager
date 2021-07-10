b29567d9a3007af2bcf7ca37404241d5f6325600
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-07-11 01:27:03 +0530

    Added signing-in, updating session, fencing of protected routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
   def index
-    render "index"
+    if current_user
+      redirect_to todos_path
+    else
+      render "index"
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      render plain: "correct password"
+      session[:current_user_id] = user.id 
+      redirect_to "/"
     else
       render plain: "incorrect"
     end
