9667c2d67aebae23c86e919d773a74cd171dc793
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-07-12 00:16:07 +0530

    Added signing-in, updating session, fencing of protected routes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,5 @@
 class TodosController < ApplicationController
   def index
-    # render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
     render "index"
   end
 
@@ -8,7 +7,6 @@ class TodosController < ApplicationController
     id = params[:id]
     todo = Todo.find(id)
     render "todo"
-    #render plain: todo.to_pleasant_string
   end
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
-skip_before_action :ensure_user_logged_in
+  skip_before_action :ensure_user_logged_in
 
-def new
-  render "users/new"
+  def new
+    render "users/new"
   end
 
   def create
     User.create!(
-    first_name: params[:first_name],
-    last_name: params[:last_name],
-    email: params[:email],
-    password: params[:password],
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
     )
-    redirect_to  "/"
+    redirect_to "/"
   end
 
   def login
