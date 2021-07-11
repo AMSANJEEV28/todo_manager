fb6574681ff063e3e33c0309bf65d2aca756a2f1
AMSANJEEV28 <amsanjeev28@gmail.com>
2021-07-12 02:39:31 +0530

    render invalid login attempt message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
       session[:current_user_id] = user.id 
       redirect_to "/"
     else
-      render plain: "incorrect"
+      flash[:error] = " Invalid Password or Email Id, Please Try Again."
+      redirect_to new_sessions_path
     end
   end
 
